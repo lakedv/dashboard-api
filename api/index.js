314540d4d78daba24b7c769043de512deeeeb15c
lakedv <uaterules@gmail.com>
2025-03-23 21:50:54 -0300

    Test2

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const admin = require("firebase-admin");
-//TEST
+
 // 🔥 Firebase Connection
 admin.initializeApp({
     credential: admin.credential.cert({
